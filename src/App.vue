d28a2d6b8056516d0b8d45acc2a681d947de5499
theta1118 <1311134412@qq.com>
2021-12-15 22:25:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+	  登录
+	  <form @submit.prevent="onSubmit">
+   <label>
+		<span>用户名</span>
+		<input type="text" v-model="user.username">
+	</label>
+	<label>
+		<span>密码</span>
+		<input type="password" v-model="user.password">
+	</label>
+	<button type="submit">登录</button>
+     </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+		 user:{
+			 username:"",
+			 password:""
+		 },
+		 x:[]
+    };
+  },
+  methods:{
+	  onSubmit(){
+		  console.log(this.user);
+	  }
+  },
+  components: {}
+};
+</script>
+
